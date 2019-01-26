d8f0f65c6b195732d8896cca663b10e93623bfdd
OleSc <schaeferole@gmx.de>
2019-01-26 07:25:35 +0100

    test

---
+++ ole/Scripts/SwitchRoom.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchRoom : MonoBehaviour
+{
+    int[] Doors;
+    // Use this for initialization
+    void Start ()
+    {
+        Doors = new int[10];
+        Doors[0] = 0;
+        Doors[1] = 100;
+        Doors[2] = 200;
+
+    }
+	public int Switcheroo(int i)
+    {
+        return Doors[i];
+    }
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
