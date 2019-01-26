8a1f3ded943950f59276d53fec15de93d640ff39
OleSc <schaeferole@gmx.de>
2019-01-26 08:28:13 +0100

    test

--- ole/Scripts/SwitchRoom.cs
+++ ole/Scripts/SwitchRoom.cs
@@ -2,25 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SwitchRoom : MonoBehaviour
+public class SwitchRoom
 {
-    int[] Doors;
+    static int[] Doors;
+    static int[] Cams;
     // Use this for initialization
-    void Start ()
+    public static void start ()
     {
         Doors = new int[10];
         Doors[0] = 0;
         Doors[1] = 100;
         Doors[2] = 200;
+        Doors[3] = 300;
+        Doors[4] = 400;
+        Doors[5] = 500;
 
     }
-	public int Switcheroo(int i)
+	public static int Switcheroo(int i)
     {
         return Doors[i];
     }
 	// Update is called once per frame
-	void Update ()
+	public static int PenisInCamera(int j)
     {
-		
-	}
+        return Cams[j];
+    }
 }
